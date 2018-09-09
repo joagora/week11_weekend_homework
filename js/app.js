901d53e85b38b26519be72cc983c039e66933acf
Joanna Gora <joanna.gora@protonmail.com>
2018-09-09 14:56:05 +0100

    add location question

--- js/app.js
+++ js/app.js
@@ -10,22 +10,51 @@ const handleFormSubmission = function(event) {
 }
 
 const createUlItem = function(form) {
-  const newDiv = document.createElement('div');
-  newDiv.setAttribute('id', 'details-container');
 
-  const body = document.querySelector('body');
-  body.appendChild(newDiv);
-
+  const divContainer = document.querySelector('#details-container');
+  const existingUls = document.querySelectorAll('#details-container ul');
+  if (existingUls.length === 0) {
+    const deleteButton = createDeleteButton();
+  }
   const singleUlItem = document.createElement('ul');
-  newDiv.appendChild(singleUlItem);
+  divContainer.appendChild(singleUlItem);
 
   const liFirstName = document.createElement('li');
-  const liLastName = document.createElement('li')
   singleUlItem.appendChild(liFirstName);
+
+  const liLastName = document.createElement('li');
   singleUlItem.appendChild(liLastName);
-  console.dir(event);
+
+  const liLocation = document.createElement('li');
+  singleUlItem.appendChild(liLocation);
+
   const firstNameValue = form.firstName.value;
-  const lastNameValue = form.lastName.value;
   liFirstName.textContent = firstNameValue;
+
+  const lastNameValue = form.lastName.value;
   liLastName.textContent = lastNameValue;
+
+  const locationValue = form.location.value;
+  liLocation.textContent = locationValue;
+
+
+}
+
+const createDeleteButton = function() {
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('id', 'delete-button');
+  deleteButton.textContent = "Delete all";
+  const body = document.querySelector('body');
+  body.appendChild(deleteButton);
+  deleteButton.addEventListener('click', handleDeletion);
+}
+
+const handleDeletion = function(event) {
+  event.preventDefault();
+  const list = document.querySelector('#details-container');
+  while(list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+  const deleteButton = event.target;
+  deleteButton.parentNode.removeChild(deleteButton);
 }
